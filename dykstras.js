254dafbb487e94b0a3244a485ea33c90ca97f105
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-05-19 16:31:25 -0400

    More es6.

--- dykstras.js
+++ dykstras.js
@@ -1,22 +1,18 @@
 const available = Number.POSITIVE_INFINITY;
 const obstructed = 'obstructed';
 
-let sourceMap = (() => {
-  const a = available;
-  const o = obstructed;
-  return [
-    [ a, a, o, a, a ],
-    [ a, a, o, a, a ],
-    [ a, a, o, o, a ],
-    [ a, a, a, a, a ],
-    [ a, o, o, a, a ],
-  ]
-})();
+let sourceMap = ((a, o) => [
+  [ a, a, o, a, a ],
+  [ a, a, o, a, a ],
+  [ a, a, o, o, a ],
+  [ a, a, a, a, a ],
+  [ a, o, o, a, a ],
+])(available, obstructed);
 let points = // These points look for an answer of [3, 3].
 [
   [2,1],
   [1,4],
-  // [4,0]
+  [4,0]
 ];
 function defined(val) {
   return typeof val !== 'undefined'
@@ -28,39 +24,23 @@ function shouldPushPoint(neighbor, distance) {
 }
 function getVisitableNeighbors(map, point, distance) {
   let neighbors = [];
-  let neighbor = map[point[0] - 1];
-  if (defined(neighbor)) neighbor = neighbor[point[1]];
-  if (shouldPushPoint(neighbor, distance)) {
-    neighbors.push([point[0] - 1, point[1]])
-  }
-  neighbor = map[point[0] + 1];
-  if (defined(neighbor) ) neighbor = neighbor[point[1]];
-  if (shouldPushPoint(neighbor, distance)) {
-    neighbors.push([point[0] + 1, point[1]])
-  }
-  neighbor = map[point[0]];
-  if (defined(neighbor) ) neighbor = neighbor[[point[1] + 1]];
-  if (shouldPushPoint(neighbor, distance)) {
-    neighbors.push([point[0], point[1] + 1])
-  }
-  neighbor = map[point[0]];
-  if (defined(neighbor) ) neighbor = neighbor[point[1] - 1];
-  if (shouldPushPoint(neighbor, distance)) {
-    neighbors.push([point[0], point[1] - 1])
+  let square;
+  for (let i = -1; i <= 1; i++) {
+    if (!map[point[0] + i]) continue;
+    for (let j = -1; j <= 1; j++) {
+      square = map[point[0] + i][point[1] + j];
+      if ((i === 0 && j === 0) || (i !== 0 && j !== 0)) continue;
+      if (!shouldPushPoint(square)) continue;
+      neighbors.push([point[0] + i, point[1] + j]);
+    }
   }
   return neighbors;
 }
 function getDistancesFromPoint(map, start) {
   let distances = [];
   let distance = 0;
-  for (let j = 0, k = map.length; j < k; j++) {
-    let down = [];
-    for (let i = 0, l = map[j].length; i < l; i++) {
-      down.push(map[j][i]);
-    }
-    distances.push(down);
-  }
-  distances[start[0]][start[1]] = 0;
+  map.map(across => distances.push([...across]));
+  distances[start[0]][start[1]] = distance;
   let neighbors = getVisitableNeighbors(map, start, distance);
 
   // map the distances
@@ -70,17 +50,12 @@ function getDistancesFromPoint(map, start) {
   // remove obstacles, visited in less moves, and off map points
   // update the distances to those points
   // start at 1) again for each point.
-
   while (neighbors.length) {
     distance++;
-    let newNeighbors = [];
-    for (var i = 0, l = neighbors.length; i < l; i++) {
-      let neighbor = neighbors[i];
+    neighbors = neighbors.reduce((acc, neighbor) => {
       distances[neighbor[0]][neighbor[1]] = distance;
-      newNeighbors = newNeighbors.concat(getVisitableNeighbors(distances, neighbor, distance));
-    }
-    neighbors = newNeighbors;
-    console.table(distances);
+      return [...acc, ...getVisitableNeighbors(distances, neighbor, distance)];
+    }, []);
   }
 
   return distances;
@@ -251,49 +226,62 @@ function mapDistances(map, points) {
 }
 let distances = mapDistances([...sourceMap], points);
 function sumTheDistances(distances) {
-  var sums = [];
-  var sum = 0;
-  for (var i = 0, across = sourceMap.length; i < across; i++) {
+  let sums = [];
+  let sum = 0;
+  sourceMap.map((across, i) => {
     sums.push([]);
-    for (var j = 0, down = sourceMap[i].length; j < down; j++) {
+    across.map((square, j) => {
       sum = 0;
-      for (var k = 0, ct = distances.length; k < ct; k++) {
-        if (distances[k][i][j] === obstructed) {
+      distances.map(distance => {
+        if (distance[i][j] === obstructed) {
           sum = obstructed;
-          continue;
+        } else {
+          sum += distance[i][j];
         }
-        sum += distances[k][i][j];
-      }
+      });
       sums[i].push(sum)
-    }
-  }
+    });
+  });
   return sums;
 }
 let distancesSums = sumTheDistances(distances);
 function findOptimalLanding(distancesSums) {
-  // assumes a single optimal point for now.
-  var optimal = {
+  let optimal = {
     points: [],
     distance: available
   };
-  var shortest = Number.POSITIVE_INFINITY;
-  for (var i = 0, across = distancesSums.length; i < across; i++) {
-    for (var j = 0, down = distancesSums[i].length; j < down; j++) {
-      if (distancesSums[i][j] > -1 && distancesSums[i][j] <= shortest) {
-        shortest = distancesSums[i][j];
+  let shortest = Number.POSITIVE_INFINITY;
+  distancesSums.map((across, i) => {
+    across.map((sum, j) => {
+      if (sum > -1 && sum <= shortest) {
+        shortest = sum;
         if (shortest < optimal.distance) {
           optimal.distance = shortest;
           optimal.points = [];
         }
         optimal.points.push([i, j]);
       }
-    }
-  }
+    });
+  });
   return optimal;
 }
 console.log(distancesSums);
 let optimalLanding = findOptimalLanding(distancesSums);
 console.log(optimalLanding);
+function isOptimalClass(distance, optimalLanding) {
+  return distance === optimalLanding.distance
+    ? 'best'
+    : '';
+}
+function isOriginSquareClass(points, row, col) {
+  return points.filter(point => (point[0] === row && point[1] === col)).length
+    ? 'origin'
+    : '';
+}
 document.body.innerHTML = `
-  ${distancesSums.map(row => row.map(col => `<div class="${col === optimalLanding.distance ? 'best' : ''}">${col}</div>`).join('')).join('')}
+  ${distancesSums.map(
+    (row, i) => row.map(
+      (square, j) => `<div class="${isOptimalClass(square, optimalLanding)} ${isOriginSquareClass(points, i, j)}">${square}</div>`
+    ).join('')
+  ).join('')}
 `;
